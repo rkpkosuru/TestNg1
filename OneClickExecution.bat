5043df64438378508a6e6890c04e68ffa2a2f6b3
rkpkosuru <rkpkosuru@gmail.com>
2017-05-25 01:12:13 -0400

    removed home dir

--- OneClickExecution.bat
+++ OneClickExecution.bat
@@ -1,3 +1,2 @@
-cd C:/Program Files (x86)/Jenkins/jobs/GitTestNg/workspace
-java -cp C:/Program Files (x86)/Jenkins/jobs/GitTestNg/workspace/bin;C:/Program Files (x86)/Jenkins/jobs/GitTestNg/workspace/Jars/* org.testng.TestNG C:/Program Files (x86)/Jenkins/jobs/GitTestNg/workspace/testng.xml
+java -cp /bin;Jars/* org.testng.TestNG testng.xml
 
